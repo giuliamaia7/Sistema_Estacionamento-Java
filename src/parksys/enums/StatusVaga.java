69ccd298892896c5a5a4b641f20265adcc961c5d
giuliamaia7 <maiagiulia10@gmail.com>
2026-06-06 16:46:29 -0300

    feat(T02): cria enum StatusVaga com constantes LIVRE OCUPADA RESERVADA

--- src/parksys/enums/StatusVaga.java
+++ src/parksys/enums/StatusVaga.java
@@ -1,5 +1,7 @@
 package parksys.enums;
 
+// enum que representa o estado atual de uma vaga do estacionamento
 public enum StatusVaga {
-
+    // cada status tem: descrição em texto e se está disponível para uso
+    LIVRE, OCUPADA, RESERVADA;
 }
