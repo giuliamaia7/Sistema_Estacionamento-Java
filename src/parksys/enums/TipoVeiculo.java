d668c58d4145c4d7fbd88ce6336a253e9c489c8d
giuliamaia7 <maiagiulia10@gmail.com>
2026-06-06 16:18:39 -0300

    feat(T01): cria enum TipoVeiculo com constantes MOTO CARRO SUV CAMINHAO

--- src/parksys/enums/TipoVeiculo.java
+++ src/parksys/enums/TipoVeiculo.java
@@ -1,5 +1,7 @@
 package parksys.enums;
 
+// enum que representa os tipos de veículo que o estacionamento aceita
 public enum TipoVeiculo {
-
+    // cada tipo tem: nome para exibir, preço por hora, e quantas vagas ocupa
+    MOTO, CARRO, SUV, CAMINHAO;
 }
